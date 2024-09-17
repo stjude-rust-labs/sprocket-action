ab4a9568dbff010f34c5a6b01528fa71e57f9ad9
Andrew Thrasher <adthrasher@gmail.com>
2024-09-17 09:45:13 -0400

    chore: improve bash checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 lint=$1
 exclusions=$2
 
-if [ ! -z "$exclusions" ]; then
+if [ -n "$exclusions" ]; then
     echo "Exclusions provided. Writing to .sprocket.yml."
     echo -n "" > .sprocket.yml
     for exclusion in $(echo $exclusions | sed 's/,/ /')
@@ -17,9 +17,13 @@ EXITCODE=0
 echo "Checking WDL files using \`sprocket lint\`."
 for file in $(find $GITHUB_WORKSPACE -name "*.wdl")
 do
-    if [ $(echo $file | grep -cvf .sprocket.yml) -eq 0 ]; then
-        echo "  [***] Excluding $file [***]"
-        continue
+    if [ -e ".sprocket.yml" ]
+    then
+        if [ $(echo $file | grep -cvf .sprocket.yml) -eq 0 ]
+        then
+            echo "  [***] Excluding $file [***]"
+            continue
+        fi
     fi
     echo "  [***] $file [***]"
     sprocket check $lint $file || EXITCODE=$(($? || EXITCODE))
